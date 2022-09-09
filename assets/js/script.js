7b89d6653b429f50f018177e008a9607ec97635b
Charles Oh <106832913+charlesoh21@users.noreply.github.com>
2022-09-09 02:20:00 -0700

    Website Start

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// TypeIt JS library
+new TypeIt("#intro-name", {
+    strings: "Hi I'm Charles!",
+    speed: 125,
+    loop: false,
+    cursor: false
+  }).go();
+
+// Jump.js library
+document.getElementById("home-jump").onclick = function() {
+    Jump("#Home");
+};
+
+document.getElementById("about-jump").onclick = function() {
+    Jump("#About");
+};
+
+document.getElementById("experience-jump").onclick = function() {
+    Jump("#Experience");
+};
+
+document.getElementById("contact-jump").onclick = function() {
+    Jump("#Contact");
+};
+
+document.getElementById("up-arrow").onclick = function() {
+    Jump("#Home");
+};
+
+document.getElementById("contact-button").onclick = function() {
+    Jump("#Contact");
+};
+
+//ScrollRevealjs library
+
+ScrollReveal().reveal('#one', {delay: 200, duration: 1000});
+
+ScrollReveal().reveal('#two', {delay: 200, duration: 1000});
+
+ScrollReveal().reveal('#about-me', {delay: 200, duration: 1000});
+
+ScrollReveal().reveal('#hobbies', {delay: 500, duration: 1000});
+
+ScrollReveal().reveal('#contact-title h2', {delay: 200, duration: 1000});
+
+ScrollReveal().reveal('#hyperlinks', {delay: 200, duration: 1000});
+
+ScrollReveal().reveal('.bottom-nav', {delay: 200, duration: 1000});
